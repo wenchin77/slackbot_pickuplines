36256d5389039e7f1e4639cacaa7b726ed9cf9d0
Wenchin <wenchin.ch@gmail.com>
2019-12-28 10:14:46 +0800

    Created slackbot with mention & message hearing functions

---
+++ README.md
@@ -0,0 +1,29 @@
+# @applepie
+
+> Simple slackbot built with Node.js and Slackbots.js that tells random pickup lines and tech quotes.
+
+## APIs Used:
+[https://github.com/BolajiAyodeji/inspireNuggets](https://github.com/BolajiAyodeji/inspireNuggets)
+
+## Slackbots (slack-bot-api)
+[https://github.com/mishk0/slack-bot-api](https://github.com/mishk0/slack-bot-api)
+
+## Quick Start
+
+``` bash
+# Install dependencies
+npm install
+
+# Serve on localhost:3000
+npm start
+
+# Create bot in Slack and generate and include your OAuth bot token
+```
+
+### Author
+
+Wenchin Chuang
+
+### Version
+
+1.0.0
---
+++ index.js
@@ -0,0 +1,136 @@
+const SlackBot = require("slackbots");
+const axios = require("axios");
+const fs = require("fs");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const bot = new SlackBot({
+  token: process.env.BOT_TOKEN,
+  name: "applepie"
+});
+
+const channel = "bot-test";
+
+const params = {
+  icon_emoji: ":apple:"
+};
+
+// Start Handler
+bot.on('start', () => {
+  bot.postMessageToChannel(
+    channel,
+    '安安我是 applepie，你今天被撩了嗎？ 還沒的話 mention 我一下呀！不然你打 "inspire me" 可以看看業界名言',
+    params
+  );
+});
+
+// Error Handler
+bot.on("error", err => console.log(err));
+
+// Message Handler
+bot.on("message", data => {
+  if (data.type == "desktop_notification") {
+    throwPickUpLine();
+    return;
+  }
+  else if (data.type !== "message") {
+    return;
+  }
+  else {
+    console.log("Handling message...");
+    handleMessage(data.text);
+    console.log(data.text);
+  }
+});
+
+function throwPickUpLine() {
+  bot.postMessageToChannel;
+  let replyData = fs.readFileSync("pickuplines.json");
+  let content = JSON.parse(replyData);
+
+  // random reply
+  const no = Math.floor(Math.random() * content.length);
+  const question = content[no].question;
+  const reply = content[no].reply;
+
+  // let params = { icon_emoji: ":smirk:" };
+
+  // if there's a question, wait for a user to type something before sending out the reply
+  if (question) {
+    bot.postMessageToChannel(channel, question, params);
+    setTimeout(replyBack(channel, reply, params), 5000); 
+    return;
+  }
+
+  // if it's only one sentence
+  bot.postMessageToChannel(channel, reply, params);
+}
+
+
+function replyBack(channel, reply, params) {
+  bot.on("message", data => {
+    if (data.type == "message") {
+      bot.postMessageToChannel(channel, reply, params);
+      return;
+    }
+  });
+}
+
+
+// Respons to Data
+function handleMessage(message) {
+  if (message.includes("ethan")) {
+    hailMyEthan();
+  } else if (message.includes("inspire me")) {
+    inspireMe();
+  } else if (message.includes("applepie")) {
+    throwPickUpLine();
+  }
+  else if (message.includes(' help')) {
+    runHelp();
+  }
+}
+
+function hailMyEthan() {
+  bot.postMessageToChannel;
+  let replyData = fs.readFileSync("content.json");
+  console.log(replyData);
+  let replies = JSON.parse(replyData);
+  console.log(replies);
+  // random reply
+  const no = Math.floor(Math.random() * replies.length);
+  const reply = replies[no].reply;
+  console.log(reply);
+  bot.postMessageToChannel(channel, reply, params);
+}
+
+// inspire Me
+function inspireMe() {
+  console.log("Running inspireMe()-----------");
+  axios
+    .get(
+      "https://raw.githubusercontent.com/BolajiAyodeji/inspireNuggets/master/src/quotes.json"
+    )
+    .then(res => {
+      const quotes = res.data;
+      const random = Math.floor(Math.random() * quotes.length);
+      const quote = quotes[random].quote;
+      const author = quotes[random].author;
+
+      bot.postMessageToChannel(channel, `:zap: ${quote} - *${author}*`, params);
+    });
+}
+
+// Show Help Text
+function runHelp() {
+  const params = {
+    icon_emoji: ":question:"
+  };
+
+  bot.postMessageToChannel(
+    channel,
+    '欠撩就打 @applepie 跟我聊聊，想看業界名言就打 "inspire me"',
+    params
+  );
+}
